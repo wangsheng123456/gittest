7ef8e85cd529785a80c95a0d01d1ee0617f3197b
Ws <Ws@athome.com>
2022-04-07 17:15:02 +0800

    first commit

---
+++ src/main/java/com/atgui/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atgui.git;
+
+/**
+ * @author shkstart
+ * @create 2022-04-07 17:10
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("Hello,Git!");
+    }
+}
